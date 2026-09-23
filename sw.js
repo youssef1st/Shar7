ee321134a099b9bf4c04d5a2d618fb389e0ef492
Irq <yossefasmajjhaha@gmail.com>
2026-09-23 22:15:19 +0300

    Add caching for assets in service worker
    Implement service worker caching for assets.

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
-self.addEventListener('fetch', e => {});
+const CACHE_NAME = 'shar8-v1';
+const assets = [
+  './',
+  './index.html',
+  './data.json',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cachedResponse => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
